0a2ddd738826456e4b5cf6358e01b948b0cea487
oskar <oskararslanov@yandex.ru>
2023-07-24 12:27:38 +0600

    add husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,43 +1,26 @@
 module.exports = {
-  root: true,
   env: {
     browser: true,
-    es6: true,
+    es2021: true,
   },
-  extends: [
-    "eslint-config-airbnb-base",
-    "airbnb/rules/react",
-    "airbnb-typescript",
-    "plugin:prettier/recommended",
-  ],
-  globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
-  },
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: "./tsconfig.json",
-    ecmaFeatures: {
-      jsx: true,
+  extends: ["standard-with-typescript", "plugin:react/recommended"],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
     },
-    ecmaVersion: 'latest',
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
     sourceType: "module",
   },
-  plugins: ["react", "@typescript-eslint", "prettier"],
-  ignorePatterns: ["*.js"],
+  plugins: ["react"],
   rules: {
-    "react/react-in-jsx-scope": "off",
-    "react/jsx-filename-extension": [1, { extensions: ["tsx"] }],
-    "react/function-component-definition": "off",
-    "react/destructuring-assignment": "off",
-    "react/prop-types": "off",
-    "react/no-this-in-sfc": "off",
-    "import/prefer-default-export": "off",
-    "prettier/prettier": "error",
-    "import/no-relative-packages": "off",
-    "import/no-extraneous-dependencies": "off",
-    "react/jsx-props-no-spreading": "off",
-    "prefer-destructuring": "off",
-    "react/require-default-props": "off",
+    "react/react-in-jsx-scope": 0,
   },
 };
